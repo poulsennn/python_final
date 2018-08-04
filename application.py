c11e267489812562ccdeed67ee16f5fcba6eb4f6
poulsennn <svendpoulsen@outlook.com>
2018-08-04 11:59:06 -0400

    Bodyfat calc with Bootstrap

--- application.py
+++ application.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def bodyfat_M(sf_sum, age):
+    res = 0.465 + 0.18*sf_sum - (0.0002406*sf_sum)**2 + 0.06619*age
+    return res
 
-def bmi(weight, height):
-    res = weight / (height ** 2)
+def bodyfat_F(sf_sum):
+    res = 13.1 + 0.12*sf_sum
     return res
 
 
@@ -14,16 +17,31 @@ calculations = []
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        height = float(request.form.get("height"))
-        weight = float(request.form.get("weight"))
-        calculated_bmi = round(bmi(weight, height), 1)
-        calculations.append(calculated_bmi)
+        input_date = request.form.get("date")
+        input_sex = request.form.get("sex")
+        input_age = int(request.form.get("age"))
+        input_weight = float(request.form.get("weight"))
+        input_pec = float(request.form.get("pectoralis"))
+        input_tri = float(request.form.get("triceps"))
+        input_sub = float(request.form.get("subscapular"))
+        input_axi = float(request.form.get("axillary"))
+        input_sup = float(request.form.get("suprailliac"))
+        input_abd = float(request.form.get("abdomen"))
+        input_thi = float(request.form.get("thigh"))
+        input_sf_sum = input_pec + input_tri + input_sub + input_axi +\
+        input_sup + input_abd + input_thi
+        if input_sex == "M":
+            bf_calc = bodyfat_M(input_sf_sum, input_age)
+        else:
+            bf_calc = bodyfat_F(input_sf_sum)
+        fat_mass = input_weight*(bf_calc/100)
+        fat_free_mass = input_weight - fat_mass
+        calculations.append([input_date, input_sex, input_age, input_weight,\
+        input_pec, input_tri, input_sub, input_axi, input_sup, input_abd,\
+        input_thi, round(input_sf_sum, 1), round(bf_calc, 1),\
+        round(fat_free_mass, 1), round(fat_mass, 1)])
     return render_template("index.html", calculations=calculations)
 
 
-@app.route("/calculate", methods=["POST"])
-def calculate():
-    height = float(request.form.get("height"))
-    weight = float(request.form.get("weight"))
-    calculated_bmi = round(bmi(weight, height), 1)
-    return render_template("calculate.html", calculated_bmi=calculated_bmi)
+if __name__ == "__main__":
+    app.run()
